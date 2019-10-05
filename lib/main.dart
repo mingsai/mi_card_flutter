59bfbca95c959462188bb47c11597a32392387f6
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-10-04 21:16:03 -0400

    edit container, padding, margins, wrap widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
+
   );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.only(left:50.0),
+            constraints: BoxConstraints.expand(
+              height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+            ),
+            padding: const EdgeInsets.all(8.0),
+            color: Colors.blue[600],
+            alignment: Alignment.center,
+            child: Text('Hello World',
+                style: Theme.of(context)
+                    .textTheme
+                    .display1
+                    .copyWith(color: Colors.white)),
+            transform: Matrix4.rotationZ(0.1),
+          ),
+        ),
+      ),
+    );
+  }
 }
