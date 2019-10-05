ca1b0df3f4375c81f0e26da9cf8fb3d9d6b354cf
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-10-04 22:08:40 -0400

    column properties

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,41 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.cyan,
         body: SafeArea(
-          child: Container(
-            margin: EdgeInsets.only(left:50.0),
-            constraints: BoxConstraints.expand(
-              height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
-            ),
-            padding: const EdgeInsets.all(8.0),
-            color: Colors.blue[600],
-            alignment: Alignment.center,
-            child: Text('Hello World',
-                style: Theme.of(context)
-                    .textTheme
-                    .display1
-                    .copyWith(color: Colors.white)),
-            transform: Matrix4.rotationZ(0.1),
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(left:50.0),
+                constraints: BoxConstraints.expand(
+                  height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+                ),
+                padding: const EdgeInsets.all(8.0),
+                color: Colors.blue[600],
+                alignment: Alignment.center,
+                child: Text('Hello World',
+                    style: Theme.of(context)
+                        .textTheme
+                        .display1
+                        .copyWith(color: Colors.white)),
+                transform: Matrix4.rotationZ(0.1),
+              ),
+              Container(width: 100.0,
+              height: 100.0,
+              color: Colors.yellow,
+                  child:Text('Another World')
+              ),
+              Container(
+                  height: 100.0,
+                  color: Colors.green,
+                  child:Text('Another World')
+              )
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+
